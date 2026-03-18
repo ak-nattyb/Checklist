c3aa51d650983130a487955670e40cdf66eafe4a
Natty Burmeister <natty@akimbo.tv>
2026-03-18 00:27:57 -0600

    removed unneccessary files

--- contexts/Checklist.tsx
+++ contexts/Checklist.tsx
@@ -16,18 +16,7 @@ interface ChecklistStore {
 export const useChecklistStore = create<ChecklistStore>()(
   persist(
     (set) => ({
-      items: [
-        { id: "1", text: "Checklist Item 1" },
-        { id: "2", text: "Checklist Item 2" },
-        { id: "3", text: "Checklist Item 3" },
-        { id: "4", text: "Checklist Item 4" },
-        { id: "5", text: "Checklist Item 5" },
-        { id: "6", text: "Checklist Item 6" },
-        { id: "7", text: "Checklist Item 7" },
-        { id: "8", text: "Checklist Item 8" },
-        { id: "9", text: "Checklist Item 9" },
-        { id: "10", text: "Checklist Item 10" },
-      ],
+      items: [],
       addItem: (text) =>
         set((state) => ({
           items: [...state.items, { id: Date.now().toString(), text }],
