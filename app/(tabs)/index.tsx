a016ac0e4d54223fd562af06243cdbd3d9146f38
Natty Burmeister <natty@akimbo.tv>
2026-03-28 15:39:20 -0600

    working on adding LongPress capabilities to CustomScrollView

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -25,6 +25,7 @@ export default function Tab() {
     >
       <CustomScrollView
         data={visibleEntries}
+        onLongPress={}
         renderItem={({ item }) =>
           item.kind === "item" ? (
             <ChecklistItem
